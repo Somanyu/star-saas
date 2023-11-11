37677b3bedb13fca45c1caf112c1b23aeeb3dac8
Somanyu Samal <somanyu.03samal@gmail.com>
2023-11-11 18:58:16 +0530

    fix: Replace `class` to `className`

--- src/app/page.js
+++ src/app/page.js
@@ -9,28 +9,26 @@ export default function Home() {
   return (
     <main className="">
 
-      <section class="overflow-hidden sm:grid sm:grid-cols-2">
-        <div class="p-8 md:p-12 lg:px-16 lg:py-24">
-          <div class="mx-auto max-w-xl ltr:sm:text-left rtl:sm:text-right">
-            <h2 class="text-3xl font-bold text-gray-900 dark:text-slate-50 md:text-5xl">
+      <section className="overflow-hidden sm:grid sm:grid-cols-2">
+        <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+          <div className="mx-auto max-w-xl ltr:sm:text-left rtl:sm:text-right">
+            <h2 className="text-3xl font-bold text-gray-900 dark:text-slate-50 md:text-5xl">
               Control every step of your business.
             </h2>
 
-            <p class="hidden text-gray-500 md:mt-4 md:block text-2xl">
+            <p className="hidden text-gray-500 md:mt-4 md:block text-2xl">
               The internet&apos;s only source for productivity to reach heights.
             </p>
 
-            <div class="mt-4 md:mt-8">
-                <Button as={Link} href="#" variant="ghost" color="success" endContent={<HiArrowNarrowRight className="text-xl" />}>
-                  Get started
-                </Button>
-              {/* <Link href="#" class="inline-block text-sm font-medium transition">
-              </Link> */}
+            <div className="mt-4 md:mt-8">
+              <Button as={Link} href="/products" variant="ghost" color="success" endContent={<HiArrowNarrowRight className="text-xl" />}>
+                Get started
+              </Button>
             </div>
           </div>
         </div>
 
-        <Image alt="Book CTA" placeholder="blur" src={CTA} class="h-56 w-full object-cover sm:h-full" />
+        <Image alt="Book CTA" placeholder="blur" src={CTA} className="h-56 w-full object-cover sm:h-full" />
       </section>
     </main>
   )
